dc6c53d046ac51d9221813f30d932db7fcd1cf54
Subekshya8686 <subekshyakayastha4@gmail.com>
2024-12-03 11:39:00 +0545

    load image view

---
+++ lib/view/load_image_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoadImageView extends StatelessWidget {
+  const LoadImageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Load Image View'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
