3b444f3e245f497eecd36767fc5e82febbfa852a
Subekshya8686 <subekshyakayastha4@gmail.com>
2024-12-04 10:54:17 +0545

    Load image

--- lib/view/load_image_view.dart
+++ lib/view/load_image_view.dart
@@ -6,10 +6,27 @@ class LoadImageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[50],
       appBar: AppBar(
         title: const Text('Load Image View'),
         centerTitle: true,
-        elevation: 0,
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * .5,
+          // width: MediaQuery.of(context).size.width * .5,
+          child: Image.asset("assets/images/image1.jpg"),
+        ),
+        // child: Center(
+        // child: SingleChildScrollView(
+        //   child: Column(
+        //     children: [
+        //       Image.asset('assets/images/image1.jpg'),
+        //       Image.asset('assets/images/image2.jpg'),
+        //     ],
+        //   ),
+        // ),
+        // ),
       ),
     );
   }
